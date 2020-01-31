d923e2907d2a2d36b292170d6d54c912e68bba70
cmcoke <minoru.cm@gmail.com>
2020-01-30 19:23:04 -0500

    first commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,26 @@
+import '../styles/styles.css'
+import 'picturefill'
+import 'lazysizes'
+import 'flickity'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// allows webpack-dev-server to be executed by allowing hot module replacements and accepts updates on the fly
+if(module.hot) {
+  module.hot.accept()
+}
